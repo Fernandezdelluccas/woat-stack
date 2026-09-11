72443da242204f143fb76e68a16b15a96901a6b8
DrNimbos <guilhermelima.meneses@gmail.com>
2026-09-11 19:47:39 -0300

    .

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const app = express();
 const PORT = 3000;
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname)));
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
+    res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.listen(PORT, () => {
